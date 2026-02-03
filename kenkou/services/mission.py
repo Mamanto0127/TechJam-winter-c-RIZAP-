902eee65f3dbc2b4a0fc8771fda14e2b9d6c72e4
A0san <takahashi841.kdgo.student@gmail.com>
2026-02-03 13:37:27 +0900

    ミッション追加

--- kenkou/services/mission.py
+++ kenkou/services/mission.py
@@ -4,3 +4,25 @@ from kenkou.models import Mission
 def get_today_missions(count=1):
     missions = list(Mission.objects.all())
     return random.sample(missions, count)
+
+AI_missions = [
+    {
+        "title": "「忍び足」階段登り", 
+        "description": "階段を使う際、音を立てないようにゆっくり、つま先に力を入れて登ります。これだけでふくらはぎとお尻への刺激が倍増します。", 
+        "reward_damage": 10,
+    }, 
+    {
+        "title": "「かかとあげ」歯磨き", 
+        "description": "歯を磨いている間だけ、かかとの上げ下げを繰り返します。第二の心臓と呼ばれるふくらはぎを鍛えて血流をアップしましょう。", 
+        "reward_damage": 10,
+    },
+    {
+        "title": "「空気椅子」スマホ", 
+        "description": "壁に背中をつけて膝を90度に曲げてキープ。SNSを1件チェックする間だけ耐えてみましょう。",
+        "reward_damage": 10, 
+    }, 
+    {
+        "title": "「4-7-8」呼吸法", 
+        "description": "4秒吸って7秒止めて8秒かけて吐き出します。これを3回繰り返すだけで、副交感神経が優位になり"
+    }
+]
