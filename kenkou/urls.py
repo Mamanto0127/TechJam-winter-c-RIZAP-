eadc36cea49029dc756ef66b1c8300b8d18260dc
Mamanto0127 <fukuyamamanato@fukuyamaaishounoMacBook-Air.local>
2026-02-03 11:47:38 +0900

    位置情報

--- kenkou/urls.py
+++ kenkou/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "kenkou"
+
 urlpatterns = [
   path('', views.index, name='index'),
+  path("location/", views.location_receive, name="location_receive"),
 ]
--- kenkou/views.py
+++ kenkou/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-import requests
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+
+def location_receive(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        lat = data.get("latitude")
+        lon = data.get("longitude")
+        time = data.get("timestamp")
+
+        print("受信した現在地:", lat, lon, time)
+
+        return JsonResponse({"status": "ok"})
+
+    return JsonResponse({"status": "ng"}, status=400)
 
 def index(request):
-    res = requests.get("https://ipinfo.io/json")
-    data = res.json()
-
-    return render(request, 'kenkou/index.html', {
-        'response': data
-    })
+    return render(request, 'kenkou/index.html')
